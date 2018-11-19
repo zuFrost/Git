1d517ad42789b91d820ceeb857a07c9e15426d39
Alex Frost <zufrost.a@gmail.com>
2018-11-19 13:01:01 +0300

    add new String

--- Java24Treiner/Unit3/Strings/src/TestString.java
+++ Java24Treiner/Unit3/Strings/src/TestString.java
@@ -2,11 +2,14 @@
 public class TestString {
 
 	public static void main(String[] args) {
+		
+		//String
 		String bestFriend = "Mary";
 		bestFriend = bestFriend + " Smith";
 		String greeting = "Hello " + bestFriend;
 		System.out.println(greeting);
 		
+		//StringBuffer
 		StringBuffer sbf = new StringBuffer();
 		sbf.append("Mary");
 		sbf.append(" Smith");
@@ -14,12 +17,23 @@ public class TestString {
 		String fullName = sbf.toString(); // Convert back to String
 		System.out.println(fullName);
 		
+		//StringBuilder
 		StringBuilder sbld = new StringBuilder(50);
 		sbld.append("Mary");
 		sbld.append(" Smith");
 		
 		fullName = sbld.toString(); // Convert back to String
 		System.out.println(fullName);
+		
+		
+		// String new
+		String friend1=new String("Mary");
+		String friend2=new String("Joe");
+		String friend3=new String("Mary");
+		
+		System.out.println(friend1);
+		System.out.println(friend2);
+		System.out.println(friend3);
 
 	}
 
