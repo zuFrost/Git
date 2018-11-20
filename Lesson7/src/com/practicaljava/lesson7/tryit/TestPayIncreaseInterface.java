0aeec476b56b3a5dca1dfe5bc3bfa071448eaf3e
Alex Frost <zufrost.a@gmail.com>
2018-11-20 17:24:02 +0300

    apply

---
+++ Lesson7/src/com/practicaljava/lesson7/tryit/TestPayIncreaseInterface.java
@@ -0,0 +1,17 @@
+package com.practicaljava.lesson7.tryit;
+
+public class TestPayIncreaseInterface {
+
+    public static void main(String[] args) {
+
+      Payable workers[] = new Payable [3];
+            workers[0] = new Employee("John");
+            workers[1] = new Contractor("Mary");
+            workers[2] = new Employee("Steve");
+              
+                   for (Payable p: workers){
+                            p.increasePay(30);
+                   }
+             } 
+} 
+
